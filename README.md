e225e461637b874eb5babb7c11643ebab9fdaece
ilyas <ilyas.niyazov@rt.ru>
2023-01-11 20:14:50 +0300

    add main and readme files

---
+++ README.md
@@ -0,0 +1,14 @@
+### FAST API 
+
+- Path ```validation params of url```
+- Query ```validation params of query```
+
+
+### uvicorn
+
+- uvicorn main:app --reload
+```
+run server with reload
+
+for install uvicorn pip install uvicorn
+```
---
+++ main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI, Path, Query
+import uvicorn
+
+
+app = FastAPI()
+
+
+@app.get("/")
+def home():
+    return {"hello": "world"}
+
+
+@app.get('/user/{user_id}')
+def get_user_by_id(user_id: int = Path(..., gt=0, description="user id in DataBase"),
+                   user_type: str = Query(..., max_length=15, description="user type"),
+                   address: str = Query(None, min_length=3, description="address of user")):
+    """
+    :param user_id: is parameter of url (required-объязательно)
+    :param user_type: is parameter of query (optional-необъязательно)
+    :param address: is parameter of query (optional-необъязательно)
+    :return: simple json
+    """
+    return {'key': user_id, 'user_type': user_type, 'address': address}
+
