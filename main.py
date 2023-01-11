0b05b51d67f14bcf5f7a4408d66c0f9a9aeaac9b
ilyas <ilyas.niyazov@rt.ru>
2023-01-11 20:22:22 +0300

    add main and readme files

--- main.py
+++ main.py
@@ -15,9 +15,9 @@ def get_user_by_id(user_id: int = Path(..., gt=0, description="user id in DataBa
                    user_type: str = Query(..., max_length=15, description="user type"),
                    address: str = Query(None, min_length=3, description="address of user")):
     """
-    :param user_id: is parameter of url (required-объязательно)
-    :param user_type: is parameter of query (optional-необъязательно)
-    :param address: is parameter of query (optional-необъязательно)
+    :param user_id: is parameter of url (required-обязательно)
+    :param user_type: is parameter of query (optional-необязательно)
+    :param address: is parameter of query (optional-необязательно)
     :return: simple json
     """
     return {'key': user_id, 'user_type': user_type, 'address': address}
