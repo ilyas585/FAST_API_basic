d0a7670f5c89447503797d74b6419ab4c241d522
ilyas <ilyas.niyazov@rt.ru>
2023-02-01 20:15:30 +0300

    add sqlite create db add CRUD operations db

---
+++ db/example1.py
@@ -0,0 +1,46 @@
+"""
+DB - is file for save data (file as tables)
+SQL - language for request to DB
+
+                    SQL & NOSQL
+SQL (для постоянного хранения, обычно таблицы) - data of objects (users, products, address, devices)   [sqlite, postgresql, oracle]
+NOSQL (для быстрого доступа, обычно формат json) - cash, tokens etc.   [Redis, MongoDB]
+
+table users
+id   username   address
+1     alex       Moscow
+2     Oleg       Nalchik
+
+table products
+id    name    price
+
+-------------------------------------------------------------
+sqlalchemy - for create tables, get data update data etc...
+DBeaver
+"""
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, FLOAT
+
+DB_URL = "sqlite:///basic_db.sqlite3"
+engine = create_engine(url=DB_URL, echo=True)  # engine for connect application with DataBase
+metadata = MetaData()
+
+user_table = Table(
+    'users',
+    metadata,
+    Column("id", Integer, primary_key=True, unique=True, autoincrement=True),
+    # primary_key - первичный ключ, unique - все id уникальный, autoincrement - авто назначение id
+    Column("username", String),
+    Column("age", Integer),
+    Column("address", String)
+)
+
+product_table = Table(
+    'products',
+    metadata,
+    Column("id", Integer, primary_key=True, unique=True, autoincrement=True),
+    # primary_key - первичный ключ, unique - все id уникальный, autoincrement - авто назначение id
+    Column("name", String),
+    Column("price", FLOAT)
+)
+
+metadata.create_all(bind=engine)
