0c819e8f957a050bef9bf8567ae37199d6de8bcc
ilyas <ilyas.niyazov@rt.ru>
2023-03-15 19:59:23 +0300

    pushh

--- tests/configuration.py
+++ tests/configuration.py
@@ -8,3 +8,4 @@ base_path = f"{os.path.dirname(os.path.abspath(__file__))}/"
 
 ACCESSED_CATALOG_ENUM = ["food", "phones", "furniture", "vehicle", "international_food"]
 ROLE_ENUM = ["admin", "seller", "expert"]
+
